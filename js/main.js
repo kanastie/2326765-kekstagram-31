89dd073e0a8a66edc4c9b5c20d7b20210a8703f2
Anastasia Kolmagorova <canastie@gmail.com>
2024-03-22 16:41:59 +0300

    Создаёт модалку, отрисовывает большую картинку с нужными данными

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 import './functions.js';
 import {similarPhotoDescription} from './mock-data.js';
 import {createUsersPhotosThumbnails} from './users-photos.js';
+import './render-full-photo.js';
+import {f} from './render-full-photo.js';
 
 const usersPhotos = similarPhotoDescription();
 createUsersPhotosThumbnails(usersPhotos);
+f(usersPhotos);
