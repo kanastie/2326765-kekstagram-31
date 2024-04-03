6070ebad3472c7519bd8364d6d170927b888d40c
Anastasia Kolmagorova <canastie@gmail.com>
2024-04-03 18:47:41 +0300

    Реализует получение данных с сервера

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 import './functions.js';
-import {createSimilarPhotosDescription} from './mock-data.js';
+// import {createSimilarPhotosDescription} from './mock-data.js';
 import {createUsersPhotosThumbnails} from './users-photos.js';
 import {renderBigPhoto} from './render-big-photo.js';
 import './upload-photo-form.js';
 import './validate-form.js';
 import './effects-control.js';
 
-const usersPhotos = createSimilarPhotosDescription();
-createUsersPhotosThumbnails(usersPhotos);
-renderBigPhoto(usersPhotos);
+// const usersPhotos = createSimilarPhotosDescription();
+// createUsersPhotosThumbnails(usersPhotos);
+// renderBigPhoto(usersPhotos);
+
+fetch('https://31.javascript.htmlacademy.pro/kekstagram/data')
+  .then((response) => response.json())
+  .then((usersPhotos) => {
+    createUsersPhotosThumbnails(usersPhotos);
+    renderBigPhoto(usersPhotos);
+  });
+
