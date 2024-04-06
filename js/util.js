daa887a8864ee2bd77de0462f211809fd07510b0
Anastasia Kolmagorova <canastie@gmail.com>
2024-04-06 20:22:57 +0300

    Удаляет ненужные функции

--- js/util.js
+++ js/util.js
@@ -1,31 +1,3 @@
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-const getRandomNumberFromRange = (min, max) => {
-  const previousValues = [];
-
-  return function() {
-    let currentValue = getRandomInteger(min, max);
-
-    if (previousValues.length >= (max - min + 1)) {
-      return null;
-    }
-
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomInteger(min, max);
-    }
-
-    previousValues.push(currentValue);
-    return currentValue;
-  };
-};
-
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 function debounce (callback, timeoutDelay = 500) {
@@ -36,9 +8,5 @@ function debounce (callback, timeoutDelay = 500) {
   };
 }
 
-
-export {getRandomInteger};
-export {getRandomNumberFromRange};
-export {getRandomArrayElement};
 export {isEscapeKey};
 export {debounce};
